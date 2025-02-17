14ea1bbc5821e08e99e4a01f0adb92ccdf25f553
Alma <altasia.ahmadi@gmail.com>
2025-02-17 11:54:41 +0800

    setup project

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from .config import Config
+
+def create_app():
+    app= Flask(__name__)
+    app.config.from_object(Config)
+    
+    #route
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+    
+    return app
